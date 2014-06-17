848b93de9e2c4abc4fbc05dc298a670d57859277
Jason R. Coombs <jaraco@jaraco.com>
2014-06-17 13:03:22 -0400

    Create pmxbot.py

---
+++ meme/pmxbot.py
@@ -0,0 +1,12 @@
+"""
+pmxbot plugin for meme library
+"""
+
+from pmxbot.core import command
+
+@command("meme")
+def meme(client, event, nick, rest):
+    """
+    todo: how does this work?
+    """
+    raise NotImplementedError()
